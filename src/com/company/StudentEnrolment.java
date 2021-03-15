a47de48858a0eecf3f7e740a87288ba2cdb2d138
thanhlongb <s3748575@rmit.edu.vn>
2021-03-15 16:25:05 +0700

    add StudentEnrolment class

---
+++ src/com/company/StudentEnrolment.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class StudentEnrolment {
+    //  student , course, semester (2021A, 2021B etc. This is a string)
+    private Student student;
+    private Course course;
+    private String semester;
+
+    public StudentEnrolment(Student student, Course course, String semester) {
+        this.student = student;
+        this.course = course;
+        this.semester = semester;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
+}
