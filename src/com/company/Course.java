7f22133aa5a4ac55401425779918289547bb5cfc
thanhlongb <s3748575@rmit.edu.vn>
2021-03-15 16:22:27 +0700

    add Course class

---
+++ src/com/company/Course.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Course {
+    private String id;
+    private String name;
+    private Integer credits;
+
+    public Course(String id, String name, Integer credits) {
+        this.id = id;
+        this.name = name;
+        this.credits = credits;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Integer credits) {
+        this.credits = credits;
+    }
+}
